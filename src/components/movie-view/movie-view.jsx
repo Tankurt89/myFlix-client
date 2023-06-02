f385e5b61d020838edce054e22597e28614cb3c8
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-06-02 14:37:58 -0400

    movie-view
    movie-view is for when you click on a movie, it will display information about the movie/director/genre as well as a populate button to return to main view.

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,41 @@
+export const MovieView = ({ movie, onBackClick }) => {
+    return (
+        <div>
+            <div>
+                <span>Title: </span>
+                <span>{movie.Title}</span>
+            </div>
+            <div>
+                <span>Description: </span>
+                <span>{movie.Description}</span>
+            </div>
+            <br></br>
+            <div>
+                <span>Genre: </span>
+                <span>{movie.Genre.Name}</span>  
+            </div>
+            <div>
+                <span>Genre Description:</span>
+                <span>{movie.Genre.Description}</span>
+            </div>
+            <br></br>
+            <div>
+                <span>Director:</span>
+                <span>{movie.Director.Name}</span>
+            </div>
+            <div>
+                <span>Bio:</span>
+                <span>{movie.Director.Bio}</span>
+            </div>
+            <div>
+                <span>Birth:</span>
+                <span>{movie.Director.Birth}</span>
+            </div>
+            <div>
+                <span>Death:</span>
+                <span>{movie.Director.Death}</span>
+            </div>
+            <button onClick={onBackClick}>Back</button>
+        </div>
+    )
+}
