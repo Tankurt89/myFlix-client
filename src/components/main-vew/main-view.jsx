b25bb5f2585109ba7f38b2b21d97e0ac693a4e36
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-06-06 16:16:27 -0400

    main-view
    Updated main-view to allow the use of loginview and signup view. Also added logout buttons that will clear all information when logging out.

--- src/components/main-vew/main-view.jsx
+++ src/components/main-vew/main-view.jsx
@@ -1,12 +1,21 @@
 import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const [user, setUser] = useState(storedUser? storedUser: null);
+    const [token, setToken] =useState(storedToken? storedToken: null);
     const [movies, setMovies] = useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
     useEffect(() => {
-        fetch("https://agile-beach-16603.herokuapp.com/movies")
+        if(!token){
+            return;}
+        fetch("https://agile-beach-16603.herokuapp.com/movies", {headers: {Authorization: 'Bearer ${token'}})
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
@@ -32,21 +41,38 @@ export const MainView = () => {
         .catch((error) => {
             console.log('Error fetching movies:', error)
         })
-    }, [])
+    }, [token])
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
+    if (!user) {
+        return (
+            <>
+            <LoginView onLoggedIn={(user, token) => {setUser(user), setToken(token)}} />
+            or
+            <SignupView />
+            </>
+        )
+    }
 
     if (selectedMovie) {
     return (
+        <>
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
         <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        </>
         );
     }
 
     if (movies.length === 0) {
-        return <div>The list is empty!</div>;
+        return(
+        <>
+        <button onClick={() => { setUser(null); setToken(null), localStorage.clear(); }}>Logout</button>
+        <div>The list is empty!</div>;
+        </>
+        );
     }
     return (
         <div>
+            <button onClick={() => { setUser(null); setToken(null), localStorage.clear() }}>Logout</button>
             {movies.map((movie) => (
                 <MovieCard
                 key={movie._id}
